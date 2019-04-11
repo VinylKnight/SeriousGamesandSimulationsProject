5130b8377b7a865bd50ce97cfe1a5b200f2f9f63
VinylKnight <roger.bucio@loop.colum.edu>
2019-04-10 22:54:14 -0500

    Begining implementation of narrative and dialogue

--- Assets/Scripts/TextNarrative.cs
+++ Assets/Scripts/TextNarrative.cs
@@ -1,18 +1,61 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class TextNarrative : MonoBehaviour
 {
+    [SerializeField]
+    private TextMeshProUGUI Narrativedisplay;
+    [SerializeField]
+    private string[] Narrative;
+    [SerializeField]
+    private string[] negativeNarrative;
+    [SerializeField]
+    private GameObject goodButton;
+    [SerializeField]
+    private GameObject badButton;
+
+    private int index;
+    public float typingSpeed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(Type());
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Narrativedisplay.text == Narrative[index])
+        {
+            goodButton.SetActive(true);
+            badButton.SetActive(true);
+        }
+
+
+    }
+    IEnumerator Type()
+    {
+        foreach (char letter in Narrative[index].ToCharArray())
+        {
+            Narrativedisplay.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+
+        }
+    }
+    public void NextNarrative()
+    {
+        goodButton.SetActive(false);
+        badButton.SetActive(false);
+
+        if (index < Narrative.Length - 1)
+        {
+            index++;
+            Narrativedisplay.text = string.Empty;
+            StartCoroutine(Type());
+        }
+        else
+        {
+            Narrativedisplay.text = string.Empty;
+        }
     }
 }
